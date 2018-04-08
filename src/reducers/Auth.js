ad657e8f0d9ea064cebff9811b4b4c0123a16c30
Minglong Ye(Company) <yeml5@chinaunicom.cn>
2018-04-08 17:37:48 +0800

    commit

--- src/reducers/Auth.js
+++ src/reducers/Auth.js
@@ -1,13 +1,20 @@
 /**
  * Created by yeml5 on 2018/4/4.
  */
-import {LOGIN_USER_REQUEST,LOGIN_USER_SUCCESS,LOGIN_USER_FAILURE} from '../actions/ActionConfig'
+import {LOGIN_USER_REQUEST,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILURE,
+    LOGIN_OA_REQUEST,
+    LOGIN_OA_SUCCESS,
+    LOGIN_OA_FAILURE,} from '../actions/ActionConfig'
 
 const initialState={
-    isAuthenticated:false,
+    isAuthenticated:true,
     isAuthenticating:false,
-    authData:null,
+    authData:{'userName':'yeml5',}
 };
+
+
 export function Auth (state=initialState,action) {
     switch (action.type){
         case LOGIN_USER_REQUEST:
@@ -16,6 +23,12 @@ export function Auth (state=initialState,action) {
             return {isAuthenticating:false,isAuthenticated:false};
         case LOGIN_USER_SUCCESS:
             return {isAuthenticating:false,isAuthenticated:true,authData:action.token};
+        case LOGIN_OA_REQUEST:
+            return {isAuthenticating:true,isAuthenticated:false};
+        case LOGIN_OA_SUCCESS:
+            return {isAuthenticating:false,isAuthenticated:true};
+        case LOGIN_OA_FAILURE:
+            return {isAuthenticating:false,isAuthenticated:false};
         default:
             return state;
     }
